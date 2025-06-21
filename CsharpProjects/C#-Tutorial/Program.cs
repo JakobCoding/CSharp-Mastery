2cfbfacf73e61c4d2510d30871f7c9e1cdfbfccf
Jake <jakechalmers86@gmail.com>
2025-06-21 15:54:37 +0800

    Updated program.cs with C# Tutorial data

--- CsharpProjects/C#-Tutorial/Program.cs
+++ CsharpProjects/C#-Tutorial/Program.cs
@@ -1,53 +1,4 @@
-﻿// dotnet run in the terminal to see output
-/*
-Random dice = new Random();
-
-int roll1 = dice.Next(1, 7);
-int roll2 = dice.Next(1, 7);
-int roll3 = dice.Next(1, 7);
-
-// roll1 = 6;
-// roll2 = 6;
-// roll3 = 6;
-
-int total = roll1 + roll2 + roll3;
-Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
-
-if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
-{
-    if ((roll1 == roll2) && (roll2 == roll3))
-    {
-        Console.WriteLine("You rolled triples! +6 bonus to total!");
-        total += 6;
-    }
-    else
-    {
-        Console.WriteLine("You rolled doubles! +2 bonus to total!");
-        total += 2;
-    }
-    Console.WriteLine($"Your total including the bonus: {total}");
-}
-
-if (total >= 16)
-{
-    Console.WriteLine("You win a new car!");
-}
-else if (total >= 10)
-{
-    Console.WriteLine("You win a new laptop!");
-}
-else if (total == 7)
-{
-    Console.WriteLine("You win a trip for two!");
-}
-else
-{
-    Console.WriteLine("You win a kitten!");
-}
-
-*/
-
-using System.Formats.Asn1;
+﻿using System.Formats.Asn1;
 
 internal class Program
 {
@@ -111,6 +62,54 @@ internal class Program
         //keeps the program window open 
         Console.ReadLine();
 
+        
+        
+        Random dice = new Random();
+
+        int roll1 = dice.Next(1, 7);
+        int roll2 = dice.Next(1, 7);
+        int roll3 = dice.Next(1, 7);
+
+        // roll1 = 6;
+        // roll2 = 6;
+        // roll3 = 6;
+
+        int total = roll1 + roll2 + roll3;
+        Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+        if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+        {
+            if ((roll1 == roll2) && (roll2 == roll3))
+            {
+                Console.WriteLine("You rolled triples! +6 bonus to total!");
+                total += 6;
+            }
+            else
+            {
+                Console.WriteLine("You rolled doubles! +2 bonus to total!");
+                total += 2;
+            }
+            Console.WriteLine($"Your total including the bonus: {total}");
+        }
+
+        if (total >= 16)
+        {
+            Console.WriteLine("You win a new car!");
+        }
+        else if (total >= 10)
+        {
+            Console.WriteLine("You win a new laptop!");
+        }
+        else if (total == 7)
+        {
+            Console.WriteLine("You win a trip for two!");
+        }
+        else
+        {
+            Console.WriteLine("You win a kitten!");
+        }
+
+        
         Random random = new Random();
         int daysUntilExpiration = random.Next(12);
         Console.WriteLine(daysUntilExpiration);
